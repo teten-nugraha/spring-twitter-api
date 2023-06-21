c849e8a7536b118a8e8f106f20301bc7a1991927
Teten Nugraha <teten.nugraha18@gmail.com>
2023-06-21 11:12:46 +0700

    feat : apply request and response for authentication

---
+++ src/main/java/id/ten/forumservices/dto/response/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package id.ten.forumservices.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
