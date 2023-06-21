a7aab74eb8c57e9433e7a2e537b08cfca6fee2a6
Teten Nugraha <teten.nugraha18@gmail.com>
2023-06-21 11:12:29 +0700

    feat : apply User Repository

---
+++ src/main/java/id/ten/forumservices/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package id.ten.forumservices.repositories;
+
+import id.ten.forumservices.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
