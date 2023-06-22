1645430186c856cf3a8e27bb368ef1e6b55f309a
Teten Nugraha <teten.nugraha18@gmail.com>
2023-06-22 11:44:20 +0700

    feat : create user service

---
+++ src/main/java/id/ten/forumservices/service/UserService.java
@@ -0,0 +1,7 @@
+package id.ten.forumservices.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+    UserDetailsService userDetailsService();
+}
