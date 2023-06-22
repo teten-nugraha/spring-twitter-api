66e62d1f63870e72e01a42f03d3a163793fd68d4
Teten Nugraha <teten.nugraha18@gmail.com>
2023-06-22 19:48:37 +0700

    feat : create string utils constant

---
+++ src/main/java/id/ten/forumservices/config/Constant.java
@@ -0,0 +1,11 @@
+package id.ten.forumservices.config;
+
+public class Constant {
+
+    public static class Rest {
+        public static String AUTHORIZATION_HEADER = "Authorization";
+        public static String BEARER = "Bearer ";
+        public static String BASE_AUTH_URL = "/api/v1/auth/**";
+        public static int AUTH_HEADER_SUBSTRING = 7;
+    }
+}
