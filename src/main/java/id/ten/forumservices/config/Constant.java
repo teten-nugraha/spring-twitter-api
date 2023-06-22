fb56ababfe0aa9a9110de504ce07ae2482338c88
Teten Nugraha <teten.nugraha18@gmail.com>
2023-06-22 19:52:44 +0700

    apply : private controller for testing

--- src/main/java/id/ten/forumservices/config/Constant.java
+++ src/main/java/id/ten/forumservices/config/Constant.java
@@ -7,5 +7,6 @@ public class Constant {
         public static String BEARER = "Bearer ";
         public static String BASE_AUTH_URL = "/api/v1/auth/**";
         public static int AUTH_HEADER_SUBSTRING = 7;
+        public static String API_V1 = "/api/v1";
     }
 }
